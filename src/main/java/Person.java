204df53d8734b9469dc51db2b3622ec575ded83b
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-09-18 20:55:47 +0200

    initial commit with Person class on maniek's branch third

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,11 @@
 public class Person {
-    private String name;
-    private String surname;
-    private Long Age;
+    private final String name;
+    private final String surname;
+    private final Long Age;
+
+    public Person(String name, String surname, Long age) {
+        this.name = name;
+        this.surname = surname;
+        Age = age;
+    }
 }
