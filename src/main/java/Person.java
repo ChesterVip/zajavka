525d59948eacdaa99eb9721cfdc7f85469808477
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-09-18 20:58:22 +0200

    initial commit with Person class on main's branch fifth

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -4,4 +4,8 @@ public class Person {
     private final String pesel;
     private final BigDecimal salary;
 
+    public Person(String pesel, BigDecimal salary) {
+        this.pesel = pesel;
+        this.salary = salary;
+    }
 }
