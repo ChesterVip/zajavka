dcb5643c342bbef3c56277bca7fea0f31efd53fc
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-11-28 20:18:09 +0100

    fixup! initial commit with Person class on main's branch sixthkj,

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,11 +1,42 @@
+import java.math.BigDecimal;
+
 public class Person {
+
     private final String name;
     private final String surname;
     private final Long Age;
+    private final String pesel;
+    private final BigDecimal salary;
 
-    public Person(String name, String surname, Long age) {
+    public Person(String name, String surname, Long age, String pesel, BigDecimal salary) {
         this.name = name;
         this.surname = surname;
-        Age = age;
+        this.Age = age;
+        this.pesel = pesel;
+        this.salary = salary;
+    }
+
+    void method(String pesel, BigDecimal salary) {
+        System.out.printf("twój Pesel to: %s a twoje wynagrodzenie to %s", pesel, salary);
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public Long getAge() {
+        return Age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
     }
 }
